323045f318c65a11b3290660f70963d5cd2d6ee4
DotWang <35402675+DotWang@users.noreply.github.com>
2023-11-01 18:33:29 +0800

    init

--- Pretraining and Finetuning/backbone/ops_dcnv3/functions/__init__.py
+++
@@ -1,7 +0,0 @@
-# --------------------------------------------------------
-# InternImage
-# Copyright (c) 2022 OpenGVLab
-# Licensed under The MIT License [see LICENSE for details]
-# --------------------------------------------------------
-
-from .dcnv3_func import DCNv3Function, dcnv3_core_pytorch
